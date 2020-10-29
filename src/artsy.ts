43dd02e75435e017b44eb7c9bcab6fd235454f9a
Scott Block <scottrblock@gmail.com>
2020-10-29 10:57:33 -0400

    cleanup

--- src/artsy.ts
+++ src/artsy.ts
@@ -1,53 +1,47 @@
 import axios from "axios";
 import { da } from "date-fns/locale";
 import { Artist, Link, Resource } from "../src/types"
-require('dotenv').config();
 
+const apiClient = axios.create({
+  baseURL: 'https://api.artsy.net/api',
+  responseType: 'json',
+  headers: { 'Content-Type': 'application/json' }
+});
 
-export const fetchToken = async () => {
-    const clientId = process.env.ARTSY_CLIENT_ID
-    const clientSecret = process.env.ARTSY_CLIENT_SECRET
-    const apiUrl = 'https://api.artsy.net/api/tokens/xapp_token';
+const fetchToken = async (): Promise<string> => (
+  await apiClient.post("/tokens/xapp_token", {
+    client_id: process.env.ARTSY_CLIENT_ID,
+    client_secret: process.env.ARTSY_CLIENT_SECRET
+  })
+    .then((result) => result.data.token)
+    .catch((error) => Promise.reject(new Error(error)))
+)
 
-    return await axios.post(apiUrl, {
-        client_id: clientId,
-        client_secret: clientSecret
+const fetchArtist = async (artist_url: string, token: string): Promise<Artist> => (
+  await apiClient.get(artist_url, { headers: { 'X-Xapp-Token': token } })
+    .then((result) => result.data)
+    .catch((error) => Promise.reject(new Error(error)))
+)
+
+export const searchArtist = async (query: string, token: string): Promise<Artist> => {
+  return await apiClient.get(`/search?q=/${query}+more:pagemap:metatags-og_type:artist`, {
+    headers: { 'X-Xapp-Token': token }
+  })
+    .then((result) => {
+      const artists = result.data['_embedded']['results'].filter((resource: Resource) => resource['type'] === 'artist')
+      if (artists.length == 0) {
+        throw new Error(`No artists found for query: ${query}`)
+      } else {
+        return fetchArtist(artists[0]['_links']['self']['href'], token)
+      }
     })
-        .then((result) => result.data.token)
-        .catch((error) => console.log('there is error', error))
-}
-
-export const fetchArtist = async (artist_url: string, token: string): Promise<void | Artist> => {
-    return await axios.get(artist_url, { headers: { 'X-Xapp-Token': token } })
-        .then((result) => result.data)
-        .catch((error) => console.error(error))
-}
-
-export const searchArtist = async (query: string, token: string): Promise<void | Artist> => {
-    return await axios.get(`https://api.artsy.net/api/search?q=/${query}+more:pagemap:metatags-og_type:artist`, {
-        headers: { 'X-Xapp-Token': token }
-    })
-        .then((result) => {
-            const artists = result.data['_embedded']['results'].filter((resource: Resource) => resource['type'] === 'artist')
-            if (artists.length == 0) {
-                throw new Error(`No artists found for query: ${query}`)
-            } else {
-                return fetchArtist(artists[0]['_links']['self']['href'], token)
-            }
-        })
-        .then((result) => result)
-        .catch((error) => console.error(error))
+    .catch((error) => Promise.reject(new Error(error)))
 }
 
 (async () => {
-    if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
-    const token = await fetchToken()
-    const artistOrVoid: Artist | void = await searchArtist("Andy Wwarhol", token)
-
-    if (typeof (artistOrVoid) === 'undefined') {
-        console.log('Unknown error')
-    } else {
-        const artist: Artist = artistOrVoid
-        return artist
-    }
+  if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
+  const token: string = await fetchToken()
+  const artist: Artist = await searchArtist("Andy Wwarhol", token)
+  console.log(artist)
+  return artist
 })()
