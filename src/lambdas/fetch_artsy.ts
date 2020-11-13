ee11f4fa8cb4f908a6b506db0e0e4e8a635f3387
Scott Block <scottrblock@gmail.com>
2020-11-13 00:46:22 -0500

    cors

--- src/lambdas/fetch_artsy.ts
+++ src/lambdas/fetch_artsy.ts
@@ -1,7 +1,12 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
 import { Artist, Resource } from "../../src/types"
 import axios from "axios";
+import { AxiosPromise } from "axios";
 
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+};
 const buildParams = (input: string): string => {
   return input
 }
@@ -12,6 +17,19 @@ const apiClient = axios.create({
   headers: { 'Content-Type': 'application/json' }
 })
 
+const wrapAxios = (req: AxiosPromise<any>) =>
+  req
+    .then((r) => ({
+      statusCode: 200,
+      body: JSON.stringify(r.data),
+      headers,
+    }))
+    .catch((e) => ({
+      statusCode: e.response?.status || 500,
+      body: e.response?.data ? JSON.stringify(e.response.data) : e.message,
+      headers,
+    }));
+
 const fetchToken = async (): Promise<string> => (
   await apiClient.post("/tokens/xapp_token", {
     client_id: process.env.ARTSY_CLIENT_ID,
@@ -21,11 +39,19 @@ const fetchToken = async (): Promise<string> => (
     .catch((error) => Promise.reject(new Error(error)))
 )
 
-const fetchArtist = async (artist_url: string, token: string): Promise<Artist> => (
+const fetchArtist = async (artist_url: string, token: string): Promise<any> => (
   await apiClient.get(artist_url, { headers: { 'X-Xapp-Token': token } })
-    .then((result) => result.data)
-    .catch((error) => Promise.reject(new Error(error)))
-)
+    .then((result) => ({
+      statusCode: 200,
+      body: JSON.stringify(result.data),
+      headers
+    }))
+    .catch((e) => ({
+      statusCode: e.response?.status || 500,
+      body: e.response?.data ? JSON.stringify(e.response.data) : e.message,
+      headers,
+    })
+))
 
 const searchArtist = async (query: string, token: string): Promise<Artist> => {
   return await apiClient.get(`/search?q=/${query}+more:pagemap:metatags-og_type:artist`, {
